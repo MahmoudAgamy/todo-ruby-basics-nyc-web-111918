a635ae1a6ee3a4b943c17fe583b8036f33c15c8a
Mahmoud Agamy <agamyv@gmail.com>
2018-11-12 23:42:00 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,14 @@
 def division(num1, num2)
+  return num1/num2
  
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
 def argue
-  
+  argue
 end
 
 def greeting
@@ -15,13 +16,14 @@ def greeting
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg)
+  puts "cheese"
+  arg
 end
